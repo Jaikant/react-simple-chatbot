c437aab4ee6047882cefebf5943684038e65e569
Jai <jaikant@gmail.com>
2020-05-09 21:22:10 +0530

    ...

--- lib/steps_components/options/Options.jsx
+++ lib/steps_components/options/Options.jsx
@@ -2,9 +2,9 @@ import styled from 'styled-components';
 
 const Options = styled.ul`
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   flex-wrap: wrap;
-  margin: 2px 0 12px 0;
+  margin: 2px 16px 12px 50px;
   padding: 0 6px;
 `;
 
